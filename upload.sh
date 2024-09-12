11a1625f0c6fbbb64b04eb963b9fc078d8c74c1b
Fantasy Renald Zhafar <60190893+LizRelDev@users.noreply.github.com>
2024-09-12 15:40:20 +0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/beryllium/qassa_*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/beryllium/ && ls qassa_*.zip)"
-      rclone copy ~/rom/out/target/product/beryllium/qassa_*.zip ghou8s-ci:beryllium -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/beryllium/ && ls qassa_*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/onclite/qassa_*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/onclite/ && ls qassa_*.zip)"
+      rclone copy ~/rom/out/target/product/onclite/qassa_*.zip LizRelDev-ci:onclite -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/onclite/ && ls qassa_*.zip) Uploaded Successfully!"
 fi
