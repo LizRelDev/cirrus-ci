a025635966a3c884975844b89ad24053ca4b61e8
Fantasy Renald Zhafar <60190893+LizRelDev@users.noreply.github.com>
2024-09-12 15:47:07 +0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 if [ -f ~/rom/out/target/product/onclite/qassa_*.zip ]; then
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/onclite/ && ls qassa_*.zip)"
-      rclone copy ~/rom/out/target/product/onclite/qassa_*.zip LizRelDev-ci:onclite -P
+      rclone copy ~/rom/out/target/product/onclite/qassa_*.zip cirrus:qassa -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/onclite/ && ls qassa_*.zip) Uploaded Successfully!"
 fi
